fb3c1cdc3746bbbde3195c6de569aaaf5a8292be
saisr05 <saisrinivas162118@gmail.com>
2024-11-12 17:10:18 +0530

    Update conn.java

--- Bankmanagementsysterm/src/conn.java
+++ Bankmanagementsysterm/src/conn.java
@@ -1,4 +1,18 @@
+package bank.management.system;
 
-public class conn {
+import java.sql.*;
 
+public class Conn {
+    Connection c;
+    Statement s;
+
+    public Conn() {
+        try {
+            // Correct JDBC URL
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystem", "root", "1234");
+            s = c.createStatement();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
 }
